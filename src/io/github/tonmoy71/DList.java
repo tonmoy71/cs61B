dfe8d9dafc5b79223290f06fb50aacc9ce04df5e
Fahim Masud Choudhury <fahim@gobd.co>
2017-02-06 23:05:22 +0600

    Adds Double Linked List implementation

---
+++ src/io/github/tonmoy71/DList.java
@@ -0,0 +1,28 @@
+package io.github.tonmoy71;
+
+/**
+ * Created by tonmoy on 05-Feb-17.
+ */
+public class DList {
+    private DListNode head;
+    private DListNode tail;
+    private int size;
+
+    public void insertFront(Object item) {
+        head = new DListNode(item, head);
+        size++;
+    }
+
+    @Override
+    public String toString() {
+        return "DList{" +
+                "head=" + head +
+                ", tail=" + tail +
+                ", size=" + size +
+                '}';
+    }
+
+    public void deleteEnd() {
+
+    }
+}
---
+++ src/io/github/tonmoy71/DListNode.java
@@ -0,0 +1,24 @@
+package io.github.tonmoy71;
+
+/**
+ * Created by tonmoy on 05-Feb-17.
+ */
+public class DListNode {
+    Object item;
+    DListNode next;
+    DListNode prev;
+
+    @Override
+    public String toString() {
+        return "DListNode{" +
+                "item=" + item +
+                ", next=" + next +
+                ", prev=" + prev +
+                '}';
+    }
+
+    public DListNode(Object item, DListNode node) {
+        this.item = item;
+        this.next = node;
+    }
+}
